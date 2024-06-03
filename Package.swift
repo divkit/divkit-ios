5df3455200b455b21ed1ff0f0db84098bd0d7c73
robot-divkit <robot-divkit@yandex-team.com>
2024-06-03 17:31:00 +0300

    Release 30.3.0 b990a1e3967fc29e1a318896683b6060a6cbc6b3

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let vgsl = (
   url: "https://github.com/yandex/vgsl.git",
   packageName: "vgsl",
-  version: Version("5.0.1")
+  version: Version("5.1.0")
 )
 
 let package = Package(
